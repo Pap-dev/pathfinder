9db3d5c5a56a4dd221086203afae2c04ca53a968
Pap-dev <pierreantoineperrier@gmail.com>
2021-04-28 16:23:53 +0200

    restructured repo

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pathfinder_engine",
+    version="0.0.1",
+    description="Pathfinder Engine",
+    author="Pierre-Antoine Perrier",
+    author_email="pierreantoineperrier@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    license="Apache 2.0"
+)
